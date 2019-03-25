5c8b1f1c40df3b3dcc2e8e7a16a890a3ac92de39
lexiilamb <37764077+lexiilamb@users.noreply.github.com>
2019-03-24 22:30:38 -0400

    New script

---
+++ Assets/Scripts/Tag/CrossoutInventory.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossoutInventory : MonoBehaviour
+{
+    public GameObject xOutTablets;
+    public GameObject xOutCalculators;
+    public GameObject xOutNotebooks;
+    public GameObject xOutLaptops;
+
+    public int xTabCounter = 0;
+    public int xCalcCounter = 0;
+    public int xNotebookCounter = 0;
+    public int xLaptopCounter = 0;
+
+    public int numToActivate = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hideAllUI(xOutTablets);
+        hideAllUI(xOutCalculators);
+        hideAllUI(xOutNotebooks);
+        hideAllUI(xOutLaptops);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void hideAllUI(GameObject objectGroup)
+    {
+        for (int i = 0; i < numToActivate; i++)
+        {
+            GameObject hideObject = objectGroup.transform.GetChild(i).gameObject;
+            hideObject.SetActive(false);
+        }
+
+    }
+
+    public void crossoutTablets()
+    {
+        xOutTablets.transform.GetChild(xTabCounter).gameObject.SetActive(true);
+        xTabCounter++;
+    }
+
+    public void crossoutCalculators()
+    {
+        xOutCalculators.transform.GetChild(xCalcCounter).gameObject.SetActive(true);
+        xCalcCounter++;
+    }
+    public void crossoutNotebooks()
+    {
+        xOutNotebooks.transform.GetChild(xNotebookCounter).gameObject.SetActive(true);
+        xNotebookCounter++;
+    }
+    public void crossoutLaptops()
+    {
+        xOutLaptops.transform.GetChild(xLaptopCounter).gameObject.SetActive(true);
+        xLaptopCounter++;
+    }
+}
