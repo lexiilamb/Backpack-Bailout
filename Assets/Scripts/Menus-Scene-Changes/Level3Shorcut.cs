a5ecd6a23ee01dfb53885c1f0289887e39fa39df
lexiilamb <37764077+lexiilamb@users.noreply.github.com>
2019-03-27 01:25:58 -0400

    Added level3 shortcut to main menu and time to level 3 canvas

---
+++ Assets/Scripts/Menus-Scene-Changes/Level3Shorcut.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level3Shorcut : MonoBehaviour
+{
+    public string scene = "Level3";
+    public Color loadToColor = Color.black;
+    public float speed = 1.0f;
+
+    public void SceneSwitcher()
+    {
+        // Load Level 1
+        Initiate.Fade(scene, loadToColor, speed);
+        //SceneManager.LoadScene(0);
+    }
+}
