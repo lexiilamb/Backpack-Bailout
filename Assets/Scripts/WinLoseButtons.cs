f36c4878cd319bf88a64ed4115390ae272ab6faa
lexiilamb <37764077+lexiilamb@users.noreply.github.com>
2019-03-27 00:38:11 -0400

    Level 3 win/loses canvases
    Added main menu and quit navigation buttons to win/lose canvases. Main menu does not work right now.

---
+++ Assets/Scripts/WinLoseButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinLoseButtons : MonoBehaviour
+{
+    public string scene = "MainMenu";
+    public Color loadToColor = Color.black;
+    public float speed = 1.0f;
+
+    public static bool GameIsPaused = false;
+    public GameObject canvasToTurnOff;
+
+    public void MainMenu()
+    {
+        // Load main menu
+        Time.timeScale = 1f;
+        canvasToTurnOff.gameObject.SetActive(false);
+        Initiate.Fade(scene, loadToColor, speed);
+    }
+
+    public void Quit()
+    {
+        // Quit Game
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
