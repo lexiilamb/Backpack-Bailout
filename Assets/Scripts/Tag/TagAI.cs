fba0f319a1f88468250b870ea556df3606fd590b
lexiilamb <37764077+lexiilamb@users.noreply.github.com>
2019-03-28 21:00:55 -0400

    Integrated Suvrat's New Chad Animator
    Suvrat now has Chad looking left and right instead of wandering when player is in safeZone.
    I modified Suvrat's script to use IEnumerators to wait between animation changes.

--- Assets/Scripts/Tag/TagAI.cs
+++ Assets/Scripts/Tag/TagAI.cs
@@ -1,39 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class TagAI : MonoBehaviour
 {
-    //walkable dimension of the room
-    //private float roomXlowerValue = -9.1f;
-    //private float roomXupperValue = 48f;
-    //private float roomZlowerValue = -9.9f;
-    //private float roomZupperValue = 16.5f;
+    private IEnumerator changeDirectionFacing;
+    public bool checkDirectionChange = true;
+    public float waitTime = 4.0f;
+    enum Animation
+    {
+        ANGRY_WALK = 0,
+        PUNCH = 1,
+        RUN = 2,
+        TURN_LEFT = 3,
+        TURN_RIGHT = 4
+    }
 
     //parameters for RandomNavSphere for NPC to wander when player is in the safe zone
     public float maxRadius = 40.0f;
 
     GameObject player;
     NavMeshAgent tagAI;
-    Animator animCon;
+    Animator ChadAnimationController;
     public bool safeZone = false;
-
-    public bool chadCaughtPlayer = false;
 
     //add view cone 
     FieldOfView target;
 
     //acceleration speeds
-    float standardSpeed = 6.0f;
-    float acceleratedSpeed = 10.0f;
+    private float stopSpeed = 0.0f;
+    private float standardSpeed = 6.0f;
+    private float acceleratedSpeed = 10.0f;
+
+    //NPC rotations
+    private bool turnedRight = false;
+
+    //win-lose state
+    public bool chadCaughtPlayer = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
 
-        animCon = GetComponent<Animator>();
+
+        ChadAnimationController = GetComponent<Animator>();
 
         if (player == null)
         {
@@ -44,61 +58,102 @@ public class TagAI : MonoBehaviour
         target = GetComponent<FieldOfView>();
     }
 
-    //new Update with view Cone implementation
+
+    //Runs every frame
     void Update()
     {
         if (player != null)
         {
-            //check if the player is in the safezone
-            if(safeZone)
-            {
-                tagAI.acceleration = standardSpeed;
-                //if the player is in the safe zone then walk around looking for the player
-                animCon.SetBool("isRunning", false);
-                animCon.SetBool("isPunchable", false);
-                animCon.SetBool("isWalkable", true);
-                tagAI.destination = RandomNavSphere(tagAI.transform.position, maxRadius);
-            }
-            else 
-            {
-                //if player is not in the safezone then check if the player is viewable
-                if(target.viewable)
-                {
-                    //if the player is viewable and within distance then punch
+            //If the target is in the safezone then peruse (look around) the room
+            //If the target is not in the safezone then check if it is within the viewcone
+            //  If the target is within the viewcone then check if NPC is within the stopping distance
+            //      If NPC is within the stopping distance then stop and punch the target
+            //      else run towards the target at an accelerated speed
+            //  If the target is not within the viewcone then walk around the room looking for the target
+            if (safeZone)
+                Peruse();
+            else
+                if (target.viewable)
                     if (tagAI.remainingDistance <= tagAI.stoppingDistance)
-                    {
-                        tagAI.acceleration = standardSpeed;
-                        animCon.SetBool("isRunning", false);
-                        animCon.SetBool("isPunchable", true);
-
-                        // Take away a life
-                        chadCaughtPlayer = true;
-                    }
-                    //if the player is viewable and not within distance then run with an accelerated speed
-                    else
-                    {
-                        tagAI.acceleration = acceleratedSpeed;
-
-                        animCon.SetBool("isRunning", true);
-                        animCon.SetBool("isPunchable", false);
-                    }
-
-                    animCon.SetBool("isWalkable", false);
-                    tagAI.destination = player.transform.position;
-                }
+                        Punch();
                 else
-                {
-                    //if player is not viewable then walk towards the player and set chad's acceleration to standard acceleration
-                    tagAI.acceleration = standardSpeed;
+                    Chase();
+            else
+                Pursue();
 
-                    animCon.SetBool("isRunning", false);
-                    animCon.SetBool("isPunchable", false);
-                    animCon.SetBool("isWalkable", true);
-                    tagAI.destination = player.transform.position;
-                }
+        }
 
+    }
+
+    IEnumerator TurnChad(int directionToTurn)
+    {
+        ChadAnimationController.SetInteger("Movement", directionToTurn);
+        Debug.Log("waiting");
+        yield return new WaitForSeconds(3.0f);
+
+        // Negate the current value of turnedRight
+        // Switches to the opposite direction to turn
+        turnedRight = !turnedRight;
+        Debug.Log(turnedRight);
+        checkDirectionChange = true;
+    }
+
+    private void Pursue()
+    {
+        //Move slow when player not visible
+        tagAI.acceleration = standardSpeed;
+
+        tagAI.transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+
+        ChadAnimationController.SetInteger("Movement", (int)Animation.ANGRY_WALK);
+
+        tagAI.destination = player.transform.position;
+    }
+
+    private void Punch()
+    {
+        //Move fast when player is visible
+        tagAI.acceleration = acceleratedSpeed;
+
+        ChadAnimationController.SetInteger("Movement", (int)Animation.PUNCH);
+
+        tagAI.destination = player.transform.position;
+
+        chadCaughtPlayer = true;
+    }
+
+    private void Chase()
+    {
+        //Move fast when player is visible
+        tagAI.acceleration = acceleratedSpeed;
+
+        ChadAnimationController.SetInteger("Movement", (int)Animation.RUN);
+
+        tagAI.destination = player.transform.position;
+    }
+
+    private void Peruse()
+    {
+        //stop chad from moving
+        tagAI.acceleration = stopSpeed;
+
+        if (checkDirectionChange)
+        {
+            checkDirectionChange = false;
+            //if the player is in the safe zone then look for the player left and right
+            if (turnedRight)
+            {
+                changeDirectionFacing = TurnChad((int)Animation.TURN_LEFT);
+                StartCoroutine(changeDirectionFacing);
+            }
+            else
+            {
+                changeDirectionFacing = TurnChad((int)Animation.TURN_RIGHT);
+                StartCoroutine(changeDirectionFacing);
             }
         }
+
+        //tagAI.destination = tagAI.transform.position;
     }
 
     //generate random point in a sphere and move to that point
@@ -115,9 +170,4 @@ public class TagAI : MonoBehaviour
 
         return navHit.position;
     }
-
-
-
-
-
 }
