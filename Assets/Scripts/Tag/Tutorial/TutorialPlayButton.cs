bd86a873ecc979fa3f02b2494d7a2df525754d0c
lexiilamb <37764077+lexiilamb@users.noreply.github.com>
2019-04-04 01:03:10 -0400

    Fixed tutorial loading issue

--- Assets/Scripts/Tag/Tutorial/TutorialPlayButton.cs
+++ Assets/Scripts/Tag/Tutorial/TutorialPlayButton.cs
@@ -12,14 +12,23 @@ public class TutorialPlayButton : MonoBehaviour
 
     public GameObject skip;
 
+    private IEnumerator stopTime;
+
     // Start is called before the first frame update
     void Start()
     {
         // Freeze game
+        stopTime = freezeTime();
+        StartCoroutine(stopTime);
+    }
+
+    IEnumerator freezeTime()
+    {
+        yield return new WaitForSeconds(1f);
         Time.timeScale = 0f;
     }
 
-    public void SkipTutorial()
+        public void SkipTutorial()
     {
         // Load level 3
         Time.timeScale = 1f;
