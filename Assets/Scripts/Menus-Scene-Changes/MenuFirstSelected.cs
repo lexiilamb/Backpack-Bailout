c58b8cda3bf819d669802f4df8960eff146d4ec4
lexiilamb <37764077+lexiilamb@users.noreply.github.com>
2019-04-29 00:10:25 -0400

    Main menu mouse click corrections

---
+++ Assets/Scripts/Menus-Scene-Changes/MenuFirstSelected.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuFirstSelected : MonoBehaviour
+{
+    public EventSystem myEventSystem;
+    public GameObject backToMenuButton;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("UH oh, mouse clicked");
+            updateFirstSelected();
+        }
+
+        if (myEventSystem.currentSelectedGameObject == null)
+        {
+            Debug.Log("Nothing selected");
+            updateFirstSelected();
+        }
+    }
+
+    // Update is called once per frame
+    public void updateFirstSelected()
+    {
+        myEventSystem.SetSelectedGameObject(backToMenuButton);
+    }
+}
--- Assets/Scripts/Menus-Scene-Changes/changeFirstSelected.cs
+++ Assets/Scripts/Menus-Scene-Changes/changeFirstSelected.cs
@@ -10,20 +10,6 @@ public class changeFirstSelected : MonoBehaviour
     public EventSystem myEventSystem;
     public GameObject backToMenuButton;
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
-        {
-            Debug.Log("UH oh, mouse clicked");
-            updateFirstSelected();
-        }
-
-        if (EventSystem.current.currentSelectedGameObject == null)
-        {
-            updateFirstSelected();
-        }
-    }
-
     // Update is called once per frame
     public void updateFirstSelected()
     {
